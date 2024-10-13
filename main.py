9bea7dbbfc6838bca314388ca97b1378dffa9460
SpellboundEcho <michaelhaerling@gmail.com>
2024-10-13 08:28:28 -0500

    added word count to main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 def main():
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    print(text)
+    book_text = get_book_text(book_path)
+    word_count = get_word_count(book_text)
+    print(f"{word_count} words were found in this document!")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def get_word_count(book_text):
+    split_text = book_text.split()
+    length_of_text = len(split_text)
+    return length_of_text
 
 main()
