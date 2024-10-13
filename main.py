d207b1416a3118133bc4bf743b466a80bf79a78b
SpellboundEcho <michaelhaerling@gmail.com>
2024-10-13 08:54:12 -0500

    added character counter to main.py

--- main.py
+++ main.py
@@ -2,15 +2,32 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     word_count = get_word_count(book_text)
-    print(f"{word_count} words were found in this document!")
+    character_dictionary = get_letter_count(book_text)
+    print(character_dictionary)
 
+# This function reads the document at the path for text
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+# This function counts the number of words in the text
 def get_word_count(book_text):
     split_text = book_text.split()
     length_of_text = len(split_text)
     return length_of_text
+
+# This function counts how many letters show up in the text
+def get_letter_count(book_text):
+    lowercase_text = book_text.lower()
+    character_count = {}
+
+    for letter in lowercase_text:
+        if letter.isalpha(): # only count alphabetic characters
+            if letter in character_count: # check if char is in the list
+                character_count[letter] += 1 # add to counter
+            else:
+                character_count[letter] = 1 # add to list
+    
+    return character_count
 
 main()
