c8c54fdc9008855f8d69a01bf15497e62ac25d40
SpellboundEcho <michaelhaerling@gmail.com>
2024-10-13 11:27:07 -0500

    add generate_report to main.py so that a report is generated

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     word_count = get_word_count(book_text)
-    character_dictionary = get_letter_count(book_text)
-    print(character_dictionary)
+    character_amount = get_letter_count(book_text)
+    generate_report(book_path, word_count, character_amount)
 
 # This function reads the document at the path for text
 def get_book_text(path):
@@ -29,5 +29,19 @@ def get_letter_count(book_text):
                 character_count[letter] = 1 # add to list
     
     return character_count
+
+# This function generates a report for the word count and character counts
+def generate_report(book_path, word_count, character_amount):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document\n")
+
+    # Sort the dictionary by values in descending order to get the most frequent characters first
+    sorted_characters = sorted(character_amount.items(), key=lambda x: x[1], reverse=True)
+
+    for char, count in sorted_characters:
+        print(f"The '{char}' character was found {count} times")
+
+    print("--- End Report ---")
+
 
 main()
